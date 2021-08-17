e9ebaf64c518927af5c7c52c40fe404965e39d3b
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-08-16 22:18:38 -0300

    Requisito 1: fazendo uma fecth de requisição na API do mercado livre, tranformar em json precorrer o objeto retornado montando um objeto menor, e adicionando como filho de uma section já criada no html

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
+
+// const fetchProduct = async () => {
+//   const data = await fetch(`${BASE_URL}/search?q=computador`);
+//   const products = await data.json();
+//   console.log(products);
+//   return products;
+// };
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,7 +21,16 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+const edgeProduct = (resultsSearch) => {
+  const productElement = {
+    sku: resultsSearch.id,
+    name: resultsSearch.title,
+    image: resultsSearch.thumbnail,
+  };
+  return productElement;
+};
+
+function createProductItemElement({ sku, name, image }) {   
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -24,12 +42,35 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+const creatList = () => {
+  const items = document.querySelector('.items');
+  const listaMl = fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+.then((response) => response.json())
+.then((resultado) => resultado.results.map((itemDoResults) => 
+edgeProduct(itemDoResults)))
+.then((resultadoFinal) => resultadoFinal.forEach((objtPeqn) => {
+  items.appendChild(createProductItemElement(objtPeqn));
+
+}));
+return listaMl;
+};
+
+
+
+
+// async function createElement(promise) {
+//   const resultado = await promise.then((result) => result);
+//   const objPqn = edgeProduct(resultado);
+//   console.log(objPqn);  
+// };
+// createElement(creatList());
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+    // escreva aqui;
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,4 +81,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+window.onload = () => {
+  creatList();
+  // fetchProduct();
+ };
